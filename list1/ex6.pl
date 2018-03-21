1486ae09f7928bfa470e1d6f247d6cd75898adfd
jakubbrodzinski <jakub.brodzinski@yahoo.com>
2018-03-21 21:19:45 +0100

    recursion -> tail recursion

--- list1/ex6.pl
+++ list1/ex6.pl
@@ -1,3 +1,5 @@
+%Check if X is prime number
+
 checkPrime(X) :-
 			X=2 ; (X>2, Y is X-1, \+ (between(2,Y,Z), (X mod Z) =:= 0)).
 prime(LO,HI,N) :- 
--- list3/ex2.pl
+++ list3/ex2.pl
@@ -1,10 +1,12 @@
 %counts the max of sum from i-th element to j-th element where i<=j and empty sum equals 0
 
-max_sum(L,MAX) :- max_sum(L,_,MAX).
+max_sum(L,MAX) :- max_sum(L,0,0,MAX).
 
-max_sum([],0,0).
-max_sum([H|R],LAST,C) :- 
-						max_sum(R,LAST2,C2),
-						NEW is H+LAST2,
-						(NEW<0 -> LAST is 0; LAST is NEW),
-						(NEW>C2 -> C is NEW; NEW=<C2, C is C2).
+max_sum([],_,A,MAX) :- MAX is A.
+						
+max_sum([H|R],LAST,A,MAX) :-NEW is H+LAST, 
+						(NEW>A -> A2 is NEW; A2 is A),
+						(NEW<0 -> 
+							max_sum(R,0,A2,MAX);
+							max_sum(R,NEW,A2,MAX)
+						).
